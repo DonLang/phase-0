aa12ff94243bf2e1c1fcb5cef5b8793d0705e733
Don Lang <donaldnlang2@gmail.com>
2015-12-04 15:08:03 -0800

    Add content to paired challenge

--- week-4/factorial/my_solution.rb
+++ week-4/factorial/my_solution.rb
@@ -4,6 +4,23 @@
 
 
 # Your Solution Below
+#Define a Method that takes a number
 def factorial(number)
-  # Your code goes here
+  #Make a variable to represent the total
+total = 1
+  #Make sure the number is greater than zero
+  if number > 0
+    # Make a counter
+  counter = 1
+    #While the counter is less than the number multiply it to the total
+  while counter <= number
+    total = total * counter
+    #increment  the counter
+    counter += 1
+  end
 end
+  #output the total
+return total
+end
+
+
--- week-4/largest_integer/my_solution.rb
+++ week-4/largest_integer/my_solution.rb
@@ -11,6 +11,23 @@
 # If +list_of_nums+ is empty the method should return nil
 
 # Your Solution Below
+#define a method largest_integer such that it takes an array of numbers
 def largest_integer(list_of_nums)
+
+
   # Your code goes here!
+  #set the default value of the largest number to nothing
+  largest= nil
+  #go through each of the numbers
+  list_of_nums.each do |num|
+    #if the there is only one number then make largest that number
+    largest ||= num
+    #test that number against others to see if they are larger
+    if num > largest
+      #if they are make them the new largest champion
+      largest = num
+    end
+  end
+  #Output the victor
+  return largest
 end
