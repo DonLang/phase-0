a0865d3decbe854f84946722991a1cce41b985be
Don Lang <donaldnlang2@gmail.com>
2015-12-04 07:29:21 -0800

    Add content to challenge files.

--- week-4/good-guess/my_solution.rb
+++ week-4/good-guess/my_solution.rb
@@ -4,3 +4,8 @@
 
 
 # Your Solution Below
+#Define good guess as taking a parameter of guess
+def good_guess?(guess)
+#Check to see if guess is equal to 42
+return guess.to_i == 42
+end
--- week-4/simple-substrings/my_solution.rb
+++ week-4/simple-substrings/my_solution.rb
@@ -1,6 +1,12 @@
 # Simple Substrings
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [  with: Everett Golden ].
 
 
 # Your Solution Below
+def welcome(address)
+  if address.include?("CA")
+    return "Welcome to California"
+  else return "You should move to California"
+  end
+end
