2d9223cb77e6022c3c46bb63635d7c928223f35e
Don Lang <donaldnlang2@gmail.com>
2015-12-06 21:12:03 -0800

    Add content to files

--- week-4/count-between/my_solution.rb
+++ week-4/count-between/my_solution.rb
@@ -18,7 +18,7 @@ def count_between(list_of_integers, lower_bound, upper_bound)
   # Your code goes here!
   #Define a variable to count the number of variables in the array between the bounds
   count=0
-  #Itterate through the array
+  #Iterate through the array
   list_of_integers.each do |check|
     #Add to the count if check is between the bounds
     if lower_bound<=check && check <=upper_bound
